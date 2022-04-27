13eb195adc603fc51ea42255d463b90e6bcda1e2
Fakhri abdillah <fakriggwp@gmail.com>
2022-04-27 07:15:11 +0700

    Add project files.

---
+++ NunitTest/TestCase.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace NunitTest
+{
+    [TestFixture]
+    class TestCase
+    {
+        [TestCase]
+        public void User()
+        {
+            Matematika mtk = new Matematika();
+            Assert.AreEqual("FakhriAbdillah", mtk.User("Fakhri"));
+        }
+
+        [TestCase]
+        public void Tambah()
+        {
+            Matematika mtk = new Matematika();
+            Assert.AreEqual(31, mtk.Tambah(20, 11));
+
+        }
+
+        [TestCase]
+        public void Kurang()
+        {
+            Matematika mtk = new Matematika();
+            Assert.AreEqual(9, mtk.Kurang(20, 11));
+
+        }
+
+
+    }
+}
